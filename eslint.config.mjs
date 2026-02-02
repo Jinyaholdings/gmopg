27f6280bca9a3e9196cab04b2b5ff7eaa616e790
Aki <mosworldfor@gmail.com>
2026-02-02 09:06:58 +0900

    ESLintのバージョンアップに伴って拡張子を変更

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        sourceType: "module",
+      },
+      globals: {
+        browser: true,
+        node: true,
+        es6: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettier,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...tseslint.configs["eslint-recommended"].rules,
+      "prettier/prettier": "error",
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+];
